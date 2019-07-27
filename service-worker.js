5d8867cbc5fd7f4b9994e69fe4043ca947843599
Connie Chan <conniechan@Connies-MBP-4.fios-router.home>
2019-07-26 21:10:43 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2f2dd80fad7f1b7c5fd0a64da88b0320.js"
+  "/precache-manifest.474734c08172475864ba5a3755b9caa4.js"
 );
 
 self.addEventListener('message', (event) => {
