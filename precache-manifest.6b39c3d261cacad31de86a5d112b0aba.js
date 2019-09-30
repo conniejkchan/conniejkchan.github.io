4e96a7cbcc5db10925d3a6a21bb5a124751def51
Connie Chan <conniechan@Connies-MBP-4.fios-router.home>
2019-09-30 02:32:34 -0400

    Updates

---
+++ precache-manifest.6b39c3d261cacad31de86a5d112b0aba.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44023801a5cc3cbd5321",
+    "url": "/static/css/main.a52e1043.chunk.css"
+  },
+  {
+    "revision": "44023801a5cc3cbd5321",
+    "url": "/static/js/main.44023801.chunk.js"
+  },
+  {
+    "revision": "45b6b7cf6a3db31c56c1",
+    "url": "/static/js/1.45b6b7cf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "faf21c953a8899b33d51a90850131758",
+    "url": "/index.html"
+  }
+];
