6981c82b79d40f97e68837ac4cfee198e3e42ee4
Connie Chan <conniechan@Connies-MBP-4.fios-router.home>
2019-09-30 01:15:29 -0400

    website update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React, { useState, useReducer } from 'react';
-import ReactDOM from 'react-dom';
-import { Layout } from 'antd';
-import Header from './components/Header/Header';
-
-import './App.css';
-
-const App = () => {
-
-  return (
-    <div className="App">
-    <Header />
-  </div>
-
-  );
-};
+import React, { Component } from 'react';
+import Header from './components/Header';
+import About from './components/About';
+import Resume from './components/Resume';
+import Portfolio from './components/Portfolio';
+import Testimonials from  './components/Testimonials';
+import ContactUs from './components/ContactUs';
+import Footer from './components/Footer';
+import resumeData from './resumeData';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Header resumeData={resumeData}/>
+        <About resumeData={resumeData}/>
+        <Resume resumeData={resumeData}/>
+        <Portfolio resumeData={resumeData}/>
+        <ContactUs resumeData={resumeData}/>
+        <Footer resumeData={resumeData}/>
+      </div>
+    );
+  }
+}
 
 export default App;
