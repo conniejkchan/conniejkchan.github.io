f3230f4a646306882cac83b702a3be26e3ef6e12
Connie Chan <conniechan@Connies-MBP-4.fios-router.home>
2019-09-30 01:53:05 -0400

    wbsite update with react

---
+++ src/resumeData.js
@@ -0,0 +1,94 @@
+let resumeData = {
+    "imagebaseurl":"https://conniejkchan.github.io/",
+    "name": "Connie Chan",
+    "role": "Software Engineer",
+    "linkedinId":"https://www.linkedin.com/",
+    "skypeid": "Your skypeid",
+    "roleDescription": " I'm currently a senior at New York University pursuing my B.A. in Computer Science with interests in full-stack software development.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"http://github.com/conniejkchan",
+          "className":"fa fa-github"
+        }
+      ],
+    "aboutme":"I'm currently a senior at New York University pursuing my B.A. in Computer Science with interests in full-stack software development.",
+    "address":"United States",
+    "website":"https://conniejkchan.github.io/",
+    "education":[
+      {
+        "UniversityName":"New York University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2020",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
