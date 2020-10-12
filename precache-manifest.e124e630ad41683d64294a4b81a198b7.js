f7cdeb9bee309fd0bd7d2cc6b623686c71c6c551
Connie Chan <conniechan@connies-mbp-4.lan>
2020-10-12 18:52:25 -0400

    Updates

---
+++ precache-manifest.e124e630ad41683d64294a4b81a198b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4ecf37af4305ae922c",
+    "url": "/static/css/main.a52e1043.chunk.css"
+  },
+  {
+    "revision": "fa4ecf37af4305ae922c",
+    "url": "/static/js/main.fa4ecf37.chunk.js"
+  },
+  {
+    "revision": "45b6b7cf6a3db31c56c1",
+    "url": "/static/js/1.45b6b7cf.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "58cad9db0b58fee8e033b007e98daba1",
+    "url": "/index.html"
+  }
+];
